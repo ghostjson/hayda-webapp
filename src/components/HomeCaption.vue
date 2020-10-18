ee91115147153b50b59168484baec65b83c59258
Akhil Ravindran <akhilravindran3@gmail.com>
2020-10-18 18:21:43 +0530

    changes in content

--- src/components/HomeCaption.vue
+++ src/components/HomeCaption.vue
@@ -5,7 +5,7 @@
                 <!-- Captions -->
                 <span data-animate="fadeInUp" data-animate-delay="300" class="strong animated fadeInUp visible" style="opacity: 1; animation-duration: 600ms;"><a href="#" class="business"><span class="business">Small quote here</span></a>
                         </span>
-                <h1 data-animate="fadeInUp" data-animate-delay="600" class="animated fadeInUp visible" style="opacity: 1; animation-duration: 600ms;">Hayda Caption Here<br>Here will be the Hayda Caption</h1>
+                <h1 data-animate="fadeInUp" data-animate-delay="600" class="animated fadeInUp visible" style="opacity: 1; animation-duration: 600ms;">HAYDA Caption Here<br>Here will be the HAYDA Caption</h1>
                 <a href="#what-we-do" data-animate="fadeInUp" data-animate-delay="900" class="btn btn-rounded btn-light animated fadeInUp visible" style="opacity: 1; animation-duration: 600ms;">Discover More</a>
                 <a data-animate="fadeInUp" data-animate-delay="1200" class="btn btn-outline btn-rounded btn-light animated fadeInUp visible" style="opacity: 1; animation-duration: 600ms;">Premium</a>
                 <!-- end: Captions -->
