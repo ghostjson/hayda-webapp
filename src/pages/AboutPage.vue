5b9741c92d5ec4eb47c70bcc2c689167f6d0e447
Akhil Ravindran <akhilravindran3@gmail.com>
2021-03-07 14:35:46 +0530

    7) Our team section added to the admin panel

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -52,119 +52,29 @@
                     <span class="lead">Create amaThe most happiest time of the day!.</span>
                 </div>
                 <div class="row team-members team-members-shadow m-b-40">
-                    <div class="col-lg-3">
+                    <div class="col-lg-3" v-for="(member, index) in about.team" :key="index">
                         <div class="team-member">
                             <div class="team-image">
-                                <img src="https://i.pravatar.cc/300?img=12">
+                                <img :src="member['image']">
                             </div>
                             <div class="team-desc">
-                                <h3>Alea Smith</h3>
-                                <span>Software Developer</span>
-                                <p>The most happiest time of the day!. Praesent tristique hendrerit ex ut laoreet. </p>
+                                <h3>{{ member['name'] }}</h3>
+                                <span>{{ member['title'] }}</span>
+                                <p>{{ member['description'] }}</p>
                                 <div class="align-center">
-                                    <a class="btn btn-xs btn-slide btn-light" href="#">
+                                    <a target="_blank" class="btn btn-xs btn-slide btn-light" :href="member['facebook']">
                                         <i class="fab fa-facebook-f"></i>
                                         <span>Facebook</span>
                                     </a>
-                                    <a class="btn btn-xs btn-slide btn-light" href="#" data-width="100">
+                                    <a target="_blank" class="btn btn-xs btn-slide btn-light" :href="member['twitter']" data-width="100">
                                         <i class="fab fa-twitter"></i>
                                         <span>Twitter</span>
                                     </a>
-                                    <a class="btn btn-xs btn-slide btn-light" href="#" data-width="118">
+                                    <a target="_blank" class="btn btn-xs btn-slide btn-light"  :href="member['instagram']" data-width="118">
                                         <i class="fab fa-instagram"></i>
                                         <span>Instagram</span>
                                     </a>
-                                    <a class="btn btn-xs btn-slide btn-light" href="mailto:#" data-width="80">
-                                        <i class="icon-mail"></i>
-                                        <span>Mail</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="team-member">
-                            <div class="team-image">
-                                <img src="https://i.pravatar.cc/300?img=5">
-                            </div>
-                            <div class="team-desc">
-                                <h3>Ariol Doe</h3>
-                                <span>Software Developer</span>
-                                <p>The most happiest time of the day!. Praesent tristique hendrerit ex ut laoreet. </p>
-                                <div class="align-center">
-                                    <a class="btn btn-xs btn-slide btn-light" href="#">
-                                        <i class="fab fa-facebook-f"></i>
-                                        <span>Facebook</span>
-                                    </a>
-                                    <a class="btn btn-xs btn-slide btn-light" href="#" data-width="100">
-                                        <i class="fab fa-twitter"></i>
-                                        <span>Twitter</span>
-                                    </a>
-                                    <a class="btn btn-xs btn-slide btn-light" href="#" data-width="118">
-                                        <i class="fab fa-instagram"></i>
-                                        <span>Instagram</span>
-                                    </a>
-                                    <a class="btn btn-xs btn-slide btn-light" href="mailto:#" data-width="80">
-                                        <i class="icon-mail"></i>
-                                        <span>Mail</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="team-member">
-                            <div class="team-image">
-                                <img src="https://i.pravatar.cc/300?img=7">
-                            </div>
-                            <div class="team-desc">
-                                <h3>Emma Ross</h3>
-                                <span>Software Developer</span>
-                                <p>The most happiest time of the day!. Praesent tristique hendrerit ex ut laoreet. </p>
-                                <div class="align-center">
-                                    <a class="btn btn-xs btn-slide btn-light" href="#">
-                                        <i class="fab fa-facebook-f"></i>
-                                        <span>Facebook</span>
-                                    </a>
-                                    <a class="btn btn-xs btn-slide btn-light" href="#" data-width="100">
-                                        <i class="fab fa-twitter"></i>
-                                        <span>Twitter</span>
-                                    </a>
-                                    <a class="btn btn-xs btn-slide btn-light" href="#" data-width="118">
-                                        <i class="fab fa-instagram"></i>
-                                        <span>Instagram</span>
-                                    </a>
-                                    <a class="btn btn-xs btn-slide btn-light" href="mailto:#" data-width="80">
-                                        <i class="icon-mail"></i>
-                                        <span>Mail</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="team-member">
-                            <div class="team-image">
-                                <img src="https://i.pravatar.cc/300?img=9">
-                            </div>
-                            <div class="team-desc">
-                                <h3>Victor Loda</h3>
-                                <span>Software Developer</span>
-                                <p>The most happiest time of the day!. Praesent tristique hendrerit ex ut laoreet. </p>
-                                <div class="align-center">
-                                    <a class="btn btn-xs btn-slide btn-light" href="#">
-                                        <i class="fab fa-facebook-f"></i>
-                                        <span>Facebook</span>
-                                    </a>
-                                    <a class="btn btn-xs btn-slide btn-light" href="#" data-width="100">
-                                        <i class="fab fa-twitter"></i>
-                                        <span>Twitter</span>
-                                    </a>
-                                    <a class="btn btn-xs btn-slide btn-light" href="#" data-width="118">
-                                        <i class="fab fa-instagram"></i>
-                                        <span>Instagram</span>
-                                    </a>
-                                    <a class="btn btn-xs btn-slide btn-light" href="mailto:#" data-width="80">
+                                    <a class="btn btn-xs btn-slide btn-light" :href="'mailto:'+member['email']" data-width="80">
                                         <i class="icon-mail"></i>
                                         <span>Mail</span>
                                     </a>
@@ -185,7 +95,27 @@
         name: 'AboutPage',
         data(){
             return {
-                about: {}
+                about: {
+                    description_left: '',
+                    description_right: '',
+                    steps: [
+                        {
+                            title: '',
+                            description: ''
+                        },
+                        {
+                            title: '',
+                            description: ''
+                        },
+                        {
+                            title: '',
+                            description: ''
+                        }
+                    ],
+                    team: [],
+
+                },
+
             }
         },
         methods: {
