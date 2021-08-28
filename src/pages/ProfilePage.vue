2f8a02bba9cab9a15f04b13eb6d15cb851c8ac6c
Akhil Ravindran <imakhilravindran@gmail.com>
2021-08-29 01:08:09 +0530

    inches to feet added

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,108 +1,171 @@
 <template>
-    <div>
-        <spinner-component v-if="loading"></spinner-component>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 m-t-60">
-                    <div class="card">
-                        <div class="card-header">
-                            <h5 class="card-title">Profile</h5>
-                        </div>
-                        <div class="card-body">
-                            <form @submit="updateProfile">
-                                <alert-box :error="error"></alert-box>
-                                <div v-if="success !== ''" class="alert alert-success" role="alert"
-                                     style="z-index: 4 !important;">
-                                    {{ success }}
-                                </div>
-                                <h6>Personal Information</h6>
-                                <div class="form-group">
-                                    <label for="zip_code">Zip Code</label>
-                                    <input required type="number" autocomplete="__away" name="zip_code"
-                                           class="form-control" v-model="profile.zip_code" id="zip_code">
-                                </div>
-                                <div class="form-group">
-                                    <label for="height">Height (inches)</label>
-                                    <input required type="number" autocomplete="__away" name="height"
-                                           class="form-control" v-model="profile.height" id="height">
-                                </div>
-                                <div class="form-group">
-                                    <label for="weight">Weight (pounds)</label>
-                                    <input required type="number" autocomplete="__away" name="weight"
-                                           class="form-control" v-model="profile.weight" id="weight">
-                                </div>
-                                <div class="form-group">
-                                    <label for="age">Age</label>
-                                    <input required type="number" autocomplete="__away" name="age" class="form-control"
-                                           v-model="profile.age" id="age">
-                                </div>
-                                <div class="form-group">
-                                    <label for="gender">Gender</label>
-                                    <select required id="gender" name="gender" class="form-control"
-                                            v-model="profile.gender">
-                                        <option value="male">Male</option>
-                                        <option value="female">Female</option>
-                                        <option value="other">Other</option>
-                                    </select>
-                                </div>
-                                <button class="btn btn-primary" type="submit">Update</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+  <div>
+    <spinner-component v-if="loading"></spinner-component>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 m-t-60">
+          <div class="card">
+            <div class="card-header">
+              <h5 class="card-title">Profile</h5>
             </div>
+            <div class="card-body">
+              <form @submit="updateProfile">
+                <alert-box :error="error"></alert-box>
+                <div
+                  v-if="success !== ''"
+                  class="alert alert-success"
+                  role="alert"
+                  style="z-index: 4 !important;"
+                >
+                  {{ success }}
+                </div>
+                <h6>Personal Information</h6>
+                <div class="form-group">
+                  <label for="zip_code">Zip Code</label>
+                  <input
+                    required
+                    type="number"
+                    autocomplete="__away"
+                    name="zip_code"
+                    class="form-control"
+                    v-model="profile.zip_code"
+                    id="zip_code"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="height">Height (inches)</label>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="input-group">
+                        <div class="input-group-prepend">
+                          <div class="input-group-text">Feet</div>
+                        </div>
+                        <input
+                          type="number"
+                          v-model="height.feet"
+                          class="form-control"
+                        />
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="input-group">
+                        <div class="input-group-prepend">
+                          <div class="input-group-text">Inches</div>
+                        </div>
+                        <input
+                          type="number"
+                          class="form-control"
+                          v-model="height.inches"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="weight">Weight (pounds)</label>
+                  <input
+                    required
+                    type="number"
+                    autocomplete="__away"
+                    name="weight"
+                    class="form-control"
+                    v-model="profile.weight"
+                    id="weight"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="age">Age</label>
+                  <input
+                    required
+                    type="number"
+                    autocomplete="__away"
+                    name="age"
+                    class="form-control"
+                    v-model="profile.age"
+                    id="age"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="gender">Gender</label>
+                  <select
+                    required
+                    id="gender"
+                    name="gender"
+                    class="form-control"
+                    v-model="profile.gender"
+                  >
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                  </select>
+                </div>
+                <button class="btn btn-primary" type="submit">Update</button>
+              </form>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
-
 <script>
-    import AlertBox from "../components/AlertBox";
-    import auth from "../services/auth";
-    import SpinnerComponent from "../components/SpinnerComponent";
+import AlertBox from "../components/AlertBox";
+import auth from "../services/auth";
+import SpinnerComponent from "../components/SpinnerComponent";
 
-    export default {
-        name: 'ProfilePage',
-        components: {SpinnerComponent, AlertBox},
-        data() {
-            return {
-                error: '',
-                success: '',
-                loading: false,
-                profile: {
-                    age: null,
-                    email: null,
-                    gender: null,
-                    height: null,
-                    name: null,
-                    weight: null,
-                    zip_code: null
-                }
-            }
-        },
-        methods: {
-            async updateProfile(e) {
-                e.preventDefault()
-                this.loading = true
-                try {
-                    await auth.updateRemoteUser(this.profile)
-                    this.success = 'Profile is successfully updated'
-                } catch (e) {
-                    console.log(e.response.error())
-                    this.error = 'Something went wrong. Try later'
-                }
-                auth.updateUser()
-                this.loading = false
-            },
-            async fetchProfile() {
-                this.loading = true
-                let response = await auth.fetchUser()
-                this.profile = response.data.data
-                this.loading = false
-            }
-        },
-        created() {
-            this.fetchProfile()
-        }
-    }
+export default {
+  name: "ProfilePage",
+  components: { SpinnerComponent, AlertBox },
+  data() {
+    return {
+      error: "",
+      success: "",
+      loading: false,
+      profile: {
+        age: null,
+        email: null,
+        gender: null,
+        height: null,
+        name: null,
+        weight: null,
+        zip_code: null,
+      },
+      height: {
+        feet: 0,
+        inches: 0,
+      },
+    };
+  },
+  methods: {
+    async updateProfile(e) {
+      e.preventDefault();
+      this.loading = true;
+      try {
+        this.profile.height = this.height.feet * 12 + this.height.inches;
+        await auth.updateRemoteUser(this.profile);
+        this.success = "Profile is successfully updated";
+      } catch (e) {
+        console.log(e.response.error());
+        this.error = "Something went wrong. Try later";
+      }
+      auth.updateUser();
+      this.loading = false;
+    },
+    async fetchProfile() {
+      this.loading = true;
+      let response = await auth.fetchUser();
+      this.profile = response.data.data;
+      this.loading = false;
+
+      this.height = {
+        feet: Math.floor(this.profile.height / 12),
+        inches: this.profile.height % 12,
+      };
+    },
+  },
+  created() {
+    this.fetchProfile();
+  },
+};
 </script>
--- src/pages/RegisterPage.vue
+++ src/pages/RegisterPage.vue
@@ -1,172 +1,279 @@
 <template>
-    <div class="container">
-        <spinner-component v-if="loader"></spinner-component>
-        <div class="row">
-            <div class="col-md-12 m-t-60">
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="card-title">Register</h5>
+  <div class="container">
+    <spinner-component v-if="loader"></spinner-component>
+    <div class="row">
+      <div class="col-md-12 m-t-60">
+        <div class="card">
+          <div class="card-header">
+            <h5 class="card-title">Register</h5>
+          </div>
+          <div class="card-body">
+            <!-- Form 1 -->
+            <form @submit="nextForm($event)" v-if="current_form === 'form1'">
+              <div class="form-group">
+                <label class="form-label">Username</label>
+                <input
+                  type="text"
+                  v-model="registerForm.name"
+                  class="form-control"
+                  placeholder="imjohn"
+                  required=""
+                />
+              </div>
+              <div class="form-group">
+                <label class="form-label">Email</label>
+                <input
+                  type="email"
+                  v-model="registerForm.email"
+                  class="form-control"
+                  placeholder="example@example.com"
+                  required=""
+                  v-on:focusout="checkEmail"
+                />
+                <small v-if="email_exist_message" class="text-danger"
+                  >Already registered email. Try
+                  <a href="/login">login</a></small
+                >
+              </div>
+              <div class="form-group">
+                <label class="form-label">Password</label>
+                <input
+                  v-on:focusout="passSet"
+                  type="password"
+                  v-model="registerForm.password"
+                  class="form-control"
+                  placeholder="*********"
+                  required=""
+                />
+                <small v-if="password_set" class="text-danger"
+                  >Password should be at least 8 characters.</small
+                >
+              </div>
+              <div class="form-group">
+                <label class="form-label">Confirm Password</label>
+                <input
+                  type="password"
+                  v-model="registerForm.confirm_password"
+                  class="form-control"
+                  placeholder="*********"
+                  required=""
+                />
+              </div>
+
+              <div class="custom-control custom-checkbox mb-4">
+                <input
+                  required
+                  type="checkbox"
+                  class="custom-control-input"
+                  id="customCheck1"
+                />
+                <label class="custom-control-label" for="customCheck1"
+                  >By signing in, you agree to our
+                  <router-link to="/terms">Terms & Conditions</router-link>
+                  and
+                  <router-link to="/terms">Privacy Statement</router-link>
+                </label>
+              </div>
+
+              <button
+                type="submit"
+                class="btn btn-primary"
+                :disabled="!correct_email"
+              >
+                Register
+              </button>
+
+              <p style="margin-top: 10px">
+                Already have an account?
+                <router-link to="/login">login</router-link>
+              </p>
+            </form>
+            <!-- End Form 1 -->
+            <!-- Form 2 -->
+            <div v-if="current_form === 'form2'">
+              <div class="form-group">
+                <input
+                  type="email"
+                  value="test@example.com"
+                  style="display: none"
+                />
+                <input type="password" value="1234" style="display: none" />
+              </div>
+              <div class="form-group">
+                <label for="zip_code">Zip Code</label>
+                <input
+                  type="number"
+                  autocomplete="__away"
+                  name="zip_code"
+                  class="form-control"
+                  v-model="registerForm.zip_code"
+                  id="zip_code"
+                />
+              </div>
+              <div class="form-group">
+                <label for="height">Height (inches)</label>
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="input-group">
+                      <div class="input-group-prepend">
+                        <div class="input-group-text">Feet</div>
+                      </div>
+                      <input
+                        type="number"
+                        v-model="height.feet"
+                        class="form-control"
+                      />
                     </div>
-                    <div class="card-body">
-                        <!-- Form 1 -->
-                        <form @submit="nextForm($event)" v-if="current_form === 'form1'">
-                            <div class="form-group">
-                                <label class="form-label">Username</label>
-                                <input type="text" v-model="registerForm.name" class="form-control" placeholder="imjohn"
-                                       required="">
-                            </div>
-                            <div class="form-group">
-                                <label class="form-label">Email</label>
-                                <input type="email" v-model="registerForm.email" class="form-control"
-                                       placeholder="example@example.com" required="" v-on:focusout="checkEmail">
-                                <small v-if="email_exist_message" class="text-danger">Already registered email. Try <a href="/login">login</a></small>
-                            </div>
-                            <div class="form-group">
-                                <label class="form-label">Password</label>
-                                <input v-on:focusout="passSet" type="password" v-model="registerForm.password" class="form-control"
-                                       placeholder="*********" required="">
-                                <small v-if="password_set" class="text-danger">Password should be at least 8 characters.</small>
-                            </div>
-                            <div class="form-group">
-                                <label class="form-label">Confirm Password</label>
-                                <input type="password" v-model="registerForm.confirm_password" class="form-control"
-                                       placeholder="*********" required="">
-                            </div>
-
-                            <div class="custom-control custom-checkbox mb-4">
-                                <input required type="checkbox" class="custom-control-input" id="customCheck1">
-                                <label class="custom-control-label" for="customCheck1">By signing in, you agree to our
-                                    <router-link to="/terms">Terms & Conditions</router-link>
-                                    and
-                                    <router-link to="/terms">Privacy Statement</router-link>
-                                </label>
-                            </div>
-
-                            <button type="submit" class="btn btn-primary" :disabled="!correct_email">Register</button>
-
-                            <p style="margin-top: 10px">Already have an account?
-                                <router-link to="/login">login</router-link>
-                            </p>
-                        </form>
-                        <!-- End Form 1 -->
-                        <!-- Form 2 -->
-                        <div v-if="current_form === 'form2'">
-                            <div class="form-group">
-                               <input type="email" value="test@example.com" style="display: none">
-                                <input type="password" value="1234" style="display: none">
-                            </div>
-                            <div class="form-group">
-                                <label for="zip_code">Zip Code</label>
-                                <input type="number" autocomplete="__away" name="zip_code" class="form-control" v-model="registerForm.zip_code" id="zip_code">
-                            </div>
-                            <div class="form-group">
-                                <label for="height">Height (inches)</label>
-                                <input type="number" autocomplete="__away" name="height" class="form-control" v-model="registerForm.height" id="height">
-                            </div>
-                            <div class="form-group">
-                                <label for="weight">Weight (pounds)</label>
-                                <input type="number" autocomplete="__away" name="weight" class="form-control" v-model="registerForm.weight" id="weight">
-                            </div>
-                            <div class="form-group">
-                                <label for="age">Age</label>
-                                <input type="number" autocomplete="__away"  name="age" class="form-control" v-model="registerForm.age" id="age">
-                            </div>
-                            <div class="form-group">
-                                <label for="gender">Gender</label>
-                                <select id="gender" name="gender" class="form-control" v-model="registerForm.gender">
-                                    <option value="male">Male</option>
-                                    <option value="female">Female</option>
-                                    <option value="other">Other</option>
-                                </select>
-                            </div>
-                            <button type="button" class="btn btn-primary" @click="registerFormSubmit">Register</button>
-                        </div>
-                        <!-- End Form 2 -->
+                  </div>
+                  <div class="col-md-6">
+                    <div class="input-group">
+                      <div class="input-group-prepend">
+                        <div class="input-group-text">Inches</div>
+                      </div>
+                      <input
+                        type="number"
+                        class="form-control"
+                        v-model="height.inches"
+                      />
                     </div>
+                  </div>
                 </div>
+              </div>
+              <div class="form-group">
+                <label for="weight">Weight (pounds)</label>
+                <input
+                  type="number"
+                  autocomplete="__away"
+                  name="weight"
+                  class="form-control"
+                  v-model="registerForm.weight"
+                  id="weight"
+                />
+              </div>
+              <div class="form-group">
+                <label for="age">Age</label>
+                <input
+                  type="number"
+                  autocomplete="__away"
+                  name="age"
+                  class="form-control"
+                  v-model="registerForm.age"
+                  id="age"
+                />
+              </div>
+              <div class="form-group">
+                <label for="gender">Gender</label>
+                <select
+                  id="gender"
+                  name="gender"
+                  class="form-control"
+                  v-model="registerForm.gender"
+                >
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                  <option value="other">Other</option>
+                </select>
+              </div>
+              <button
+                type="button"
+                class="btn btn-primary"
+                @click="registerFormSubmit"
+              >
+                Register
+              </button>
             </div>
+            <!-- End Form 2 -->
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import Auth from "../services/auth";
-    import SpinnerComponent from "../components/SpinnerComponent";
+import Auth from "../services/auth";
+import SpinnerComponent from "../components/SpinnerComponent";
 
-    export default {
-        name: 'RegisterPage',
-        components: {SpinnerComponent},
-        data() {
-            return {
-                registerForm: {
-                    name: '',
-                    email: '',
-                    password: '',
-                    confirm_password: '',
-                    zip_code: '',
-                    height: '',
-                    weight: '',
-                    age: '',
-                    gender: ''
-                },
-                current_form: 'form1',
-                loader: false,
-                correct_email: false,
-                email_exist_message: false,
-                password_set: false
-            }
-        },
-        methods: {
-            nextForm(e) {
-                e.preventDefault()
+export default {
+  name: "RegisterPage",
+  components: { SpinnerComponent },
+  data() {
+    return {
+      registerForm: {
+        name: "",
+        email: "",
+        password: "",
+        confirm_password: "",
+        zip_code: "",
+        height: "",
+        weight: "",
+        age: "",
+        gender: "",
+      },
+      height: {
+        feet: null,
+        inches: null,
+      },
+      current_form: "form1",
+      loader: false,
+      correct_email: false,
+      email_exist_message: false,
+      password_set: false,
+    };
+  },
+  methods: {
+    nextForm(e) {
+      e.preventDefault();
 
-                // password check
-                if (this.registerForm.password.length < 8){
-                    this.password_set = true
-                    return
-                }
+      // password check
+      if (this.registerForm.password.length < 8) {
+        this.password_set = true;
+        return;
+      }
 
-                if (this.current_form === 'form1') {
-                    this.current_form = 'form2'
-                }
-            },
-            async checkEmail(){
-                let is_exist = await Auth.isEmailExist(this.registerForm.email)
-                if (is_exist === 'TRUE'){
-                    // email exist
-                    this.email_exist_message = true
-                    this.correct_email = false
-                }else {
-                    // email not exist
-                    this.correct_email = true
-                    this.email_exist_message = false
-                }
-            },
-            passSet(){
-                if (this.registerForm.password.length < 8){
-                    this.password_set = true
-                }else{
-                    this.password_set = false
-                }
-            },
-            async registerFormSubmit() {
-                let status = await Auth.register(this.registerForm)
+      if (this.current_form === "form1") {
+        this.current_form = "form2";
+      }
+    },
+    async checkEmail() {
+      let is_exist = await Auth.isEmailExist(this.registerForm.email);
+      if (is_exist === "TRUE") {
+        // email exist
+        this.email_exist_message = true;
+        this.correct_email = false;
+      } else {
+        // email not exist
+        this.correct_email = true;
+        this.email_exist_message = false;
+      }
+    },
+    passSet() {
+      if (this.registerForm.password.length < 8) {
+        this.password_set = true;
+      } else {
+        this.password_set = false;
+      }
+    },
+    async registerFormSubmit() {
+      this.registerForm.height = this.height.feet * 12 + this.height.inches;
 
-                // password check
-                if (this.registerForm.password.length < 8){
-                    this.password_set = true
-                    return
-                }
+      let status = await Auth.register(this.registerForm);
 
-                if (status === 200) {
-                    this.loader = true
-                    await this.$router.push({name: 'Home'})
-                    this.loader = false
-                    location.reload()
-                } else {
-                    this.error = 'Error in registration'
-                }
-            }
-        }
-    }
+      // password check
+      if (this.registerForm.password.length < 8) {
+        this.password_set = true;
+        return;
+      }
+
+      if (status === 200) {
+        this.loader = true;
+        await this.$router.push({ name: "Home" });
+        this.loader = false;
+        location.reload();
+      } else {
+        this.error = "Error in registration";
+      }
+    },
+  },
+};
 </script>
